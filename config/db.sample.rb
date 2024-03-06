6d7c7c700c8d4b21e9a0fc10a2bcc6679df3205e
Hayden <hwalker@windevsystems.us>
2024-03-06 21:14:51 +0000

    Add db and stuff

---
+++ config/db.sample.rb
@@ -0,0 +1,14 @@
+module DCDatabase
+
+    # Postgres host
+    Host = ""
+    # Postgres port (usually 5432)
+    Port = ""
+    # Username
+    Username = ""
+    # Password
+    Password = ""
+    # Database name
+    DBName = ""
+
+end
